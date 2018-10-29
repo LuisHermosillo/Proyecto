fb194a907d078257deaabd61ade263524c368310
LuisHermosillo <42790223+LuisHermosillo@users.noreply.github.com>
2018-10-29 11:11:13 -0700

    Guardado

---
+++ JavaApplication96/src/javaapplication96/JavaApplication96.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication96;
+
+/**
+ *
+ * @author PorteoMazatlan
+ */
+public class JavaApplication96 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo en github");
+    }
+    
+}
